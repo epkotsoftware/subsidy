2fff3d41a6ecf87e5792341791254b0004ea972b
riki-araki <95273404+riki-araki@users.noreply.github.com>
2022-04-28 18:21:28 +0900

    先方指定のメールアドレスに変更

--- docs/js/script.js
+++ docs/js/script.js
@@ -14,7 +14,7 @@ const sendDoc = (grant_c_mail,text) => {
 const btn = document.getElementById('send_Doc');
 
 btn.addEventListener('click', function(event) {
-    const grant_c_mail = "szn0125@gmail.com"
+    const grant_c_mail = "support@grant-c.com"
 
     const company = document.getElementById("v9_1").value
     if (company === '') {
